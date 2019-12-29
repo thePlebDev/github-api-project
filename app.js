48b96a680bfce1ed5b87e35c7967b0c620cd932e
Tristan <tristanelliott7@gmail.com>
2019-12-29 11:56:15 -0800

    my final commit for this project. it has minor error handeling but now i will move on to a more robust fronend project.

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ function getInfo(){
 	if (inputs.value === ''){
 		message();
 	}else{
-		console.log(inputs.value)
+		fetching(inputs.value)
+
 	}
 
 	inputs.value = ''
@@ -16,4 +17,21 @@ function message(){
 	 const p = document.getElementById('message')
 	 p.appendChild(document.createTextNode('please enter a username'))
 	
+}
+
+async function fetching(value){
+	const api_url = `https://api.github.com/users/${value}`
+	const data = await fetch(api_url)// the await means that you have to wait for the promise to resolve 
+	if (!data.ok){
+		alert(' the user was not found. Please enter a different user')
+	}else{
+	const jsonData = await data.json();
+	console.log(jsonData)
+	const ul = document.getElementById('another');
+	const li = document.createElement('li');
+	li.appendChild(document.createTextNode(jsonData.login))
+	ul.appendChild(li)
+}
+
+
 }
